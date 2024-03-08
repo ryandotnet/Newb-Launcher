70fe78412208e3a2f1c427cd81e89ed48518e871
Ryan Huynh <35957436+ryandotnet@users.noreply.github.com>
2024-03-09 04:55:58 +0800

    Add files via upload

---
+++ Newb Launcher/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Newb Launcher")]
+[assembly: AssemblyDescription("MapleStory Launcher")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("VirtualSmile")]
+[assembly: AssemblyProduct("Newb Launcher")]
+[assembly: AssemblyCopyright("VirtualSmile Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("45fddbd5-e53f-4f14-98fb-5e66450bfbdb")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.0.0.1")]
+[assembly: AssemblyFileVersion("2.0.0.1")]
