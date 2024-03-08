efd1970f1250fde6bd59c95872b7e983e7bb90f3
Ryan Huynh <35957436+ryandotnet@users.noreply.github.com>
2024-03-09 04:57:39 +0800

    Add files via upload

---
+++ Newb Launcher/Request.cs
@@ -0,0 +1,95 @@
+﻿using RestSharp;
+using System.Threading.Tasks;
+
+
+namespace Newb_Launcher
+{
+    public static class Request
+    {
+        // Request Account API.
+        public static async Task<string> accountsAPIRequest(string path, RestSharp.Method method, object body)
+        {
+            RestClient client = new RestClient("https://www.nexon.com/");
+            RestRequest request = new RestRequest(path, method);
+
+            client.UserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) NexonLauncher/1.0.1 Chrome/58.0.3029.110 Electron/1.7.13 Safari/537.36";
+
+            request.AddHeader("Accept", "application/json, text/javascript, */*; q=0.01");
+            request.AddHeader("Origin", "https://www.nexon.com");
+            request.AddHeader("Content-Type", "application/json");
+            request.AddHeader("Accept-Encoding", "gzip, deflate");
+            request.AddHeader("Accept-Language", "en-US");
+            request.AddJsonBody(body);
+
+            var result = await client.ExecuteTaskAsync(request);
+
+            return result.Content;
+        }
+
+
+        // Legacy Login Request.
+        public static async Task<string> performLegacyLogin(string username, string password)
+        {
+            return await accountsAPIRequest("/account/login/legacy", Method.POST, new { id = username, password = password });
+        }
+
+        // Migration Request.
+        public static async Task<string> performMigration(string username, string password, string email)
+        {
+            return await accountsAPIRequest("/account/migrate", Method.POST, new { id = username, password = password, new_id = email });
+        }
+
+        // Login Request.
+        public static async Task<string> performLogin(string username, string password)
+        {
+            return await accountsAPIRequest("/account-webapi/login/launcher", Method.POST, new { id = username, password = Misc.SHA512(password), auto_login = false, client_id = "7853644408", scope = "us.launcher.all", device_id = Misc.SHA256(username + password) });
+        }
+
+        // Trusted Device Error Request.
+        public static async Task<string> trusted_devices(string username, string password, string code)
+        {
+            return await accountsAPIRequest("/account-webapi/trusted_devices", Method.PUT, new { email = username, remember_me = true, device_id = Misc.SHA256(username + password), verification_code = code });
+        }
+
+
+        // Nexon API Request.
+        public static async Task<string> nexonAPIRequest(string path, RestSharp.Method method, string token)
+        {
+            var client = new RestClient("https://api.nexon.io/");
+            var request = new RestRequest(path, method);
+
+            client.UserAgent = "NexonLauncher.nxl-18.12.02-407-773e6dd";
+
+            request.AddHeader("Authorization", "bearer " + System.Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(token)));
+            request.AddParameter("nxtk", token, ParameterType.Cookie);
+            request.AddParameter("domain", ".nexon.net", ParameterType.Cookie);
+            request.AddParameter("path", "/", ParameterType.Cookie);
+            request.AddHeader("Content-Type", "application/json");
+            request.AddHeader("Accept", "");
+
+            var result = await client.ExecuteTaskAsync(request);
+
+            return result.Content;
+        }
+
+        // Request Passport.
+        public static async Task<string> requestPassport(string token)
+        {
+            return await nexonAPIRequest("/users/me/passport", Method.GET, token);
+        }
+
+        public static async Task<string> passportRefresh(string authserv, string passport)
+        {
+            var client = new RestClient("http://" + authserv + ".nexon.net/");
+            var request = new RestRequest("/ajax/default.aspx?_vb=UpdateSession", Method.GET);
+
+            client.UserAgent = "Python-urllib/2.7";
+
+            request.AddParameter("NPPv2", passport, ParameterType.Cookie);
+
+            var result = await client.ExecuteTaskAsync(request);
+
+            return result.Content;
+        }
+    }
+}
---
+++ Newb Launcher/Serialize.cs
@@ -0,0 +1,101 @@
+﻿using System.IO;
+using System.Security.Cryptography;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Newb_Launcher
+{
+    class Serialize
+    {
+        // Serialized "Password"
+        private static readonly byte[] SALT = new byte[] { 0x26, 0xdc, 0xff, 0x00, 0xad, 0xed, 0x7a, 0xee, 0xc5, 0xfe, 0x07, 0xaf, 0x4d, 0x08, 0x22, 0x3c };
+
+
+        // Encrypt Function.
+        public static byte[] Encrypt(byte[] plain, string password)
+        {
+            MemoryStream memoryStream;
+            CryptoStream cryptoStream;
+
+            Rijndael rijndael = Rijndael.Create();
+            Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(password, SALT);
+
+            rijndael.Key = pdb.GetBytes(32);
+            rijndael.IV = pdb.GetBytes(16);
+
+            memoryStream = new MemoryStream();
+            cryptoStream = new CryptoStream(memoryStream, rijndael.CreateEncryptor(), CryptoStreamMode.Write);
+
+            cryptoStream.Write(plain, 0, plain.Length);
+            cryptoStream.Close();
+
+            return memoryStream.ToArray();
+        }
+
+        // Decrypt Function.
+        public static byte[] Decrypt(byte[] cipher, string password)
+        {
+            MemoryStream memoryStream;
+            CryptoStream cryptoStream;
+
+            Rijndael rijndael = Rijndael.Create();
+            Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(password, SALT);
+
+            rijndael.Key = pdb.GetBytes(32);
+            rijndael.IV = pdb.GetBytes(16);
+
+            memoryStream = new MemoryStream();
+            cryptoStream = new CryptoStream(memoryStream, rijndael.CreateDecryptor(), CryptoStreamMode.Write);
+
+            cryptoStream.Write(cipher, 0, cipher.Length);
+            cryptoStream.Close();
+
+            return memoryStream.ToArray();
+        }
+
+
+        // Serialize Function.
+        public static byte[] accountSerialize(Account account)
+        {
+            BinaryFormatter bFormat = new BinaryFormatter();
+            MemoryStream mStream = new MemoryStream();
+
+            bFormat.Serialize(mStream, account);
+
+            return mStream.GetBuffer();
+        }
+
+        // Deserialize Function.
+        public static Account accountDeserialize(byte[] buffer)
+        {
+            Account account;
+
+            BinaryFormatter bFormat = new BinaryFormatter();
+            MemoryStream mStream = new MemoryStream(buffer);
+
+            account = (Account)bFormat.Deserialize(mStream);
+
+            return account;
+        }
+
+
+        // Load Account from File Function.
+        public static Account loadAccount(string filename)
+        {
+            byte[] buffer = File.ReadAllBytes(filename);
+
+            buffer = Decrypt(buffer, "cYjo3QUvPuBu8mKR90qF");
+
+            return accountDeserialize(buffer);
+        }
+
+        // Save Account to File Function.
+        public static void saveAccount(Account account, string filename)
+        {
+            byte[] buffer = accountSerialize(account);
+
+            buffer = Encrypt(buffer, "cYjo3QUvPuBu8mKR90qF");
+
+            File.WriteAllBytes(filename, buffer);
+        }
+    }
+}
